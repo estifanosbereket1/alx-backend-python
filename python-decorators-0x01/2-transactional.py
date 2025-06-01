eea58e817ea504856093956b34e621bd692e8d08
Estifanos <estifanosbereket297@gmail.com>
2025-06-01 12:05:42 +0300

    3rd task

---
+++ python-decorators-0x01/2-transactional.py
@@ -0,0 +1,33 @@
+import sqlite3 
+import functools
+
+"""your code goes here"""
+
+def transactional(func):
+    def wrapper(*args, **kwargs):
+        conn = args[0]
+        try:
+            conn.commit()
+        except sqlite3.Error as e:
+            conn.rollback()
+    return wrapper
+
+def with_db_connection(func):
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect("users.db")  
+        try:
+            return func(conn, *args, **kwargs) 
+        finally:
+            conn.close() 
+    return wrapper
+
+
+
+@with_db_connection 
+@transactional 
+def update_user_email(conn, user_id, new_email): 
+    cursor = conn.cursor() 
+    cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id)) 
+#### Update user's email with automatic transaction handling 
+
+update_user_email(user_id=1, new_email='Crawford_Cartwright@hotmail.com')
