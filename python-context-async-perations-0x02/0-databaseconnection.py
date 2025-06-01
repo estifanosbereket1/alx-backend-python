1204a5868eeb44e11e7b3929f5093b49b843a143
Estifanos <estifanosbereket297@gmail.com>
2025-06-01 13:06:44 +0300

    context manager

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+class DatabaseConnection:
+    def __init__(self, db_name):
+        self.db_name=db_name
+        self.conn=None
+        pass
+
+    def __enter__(self):
+        self.conn=sqlite3.connect()
+        return self.conn
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.conn:
+            self.conn.close()
+        return False
