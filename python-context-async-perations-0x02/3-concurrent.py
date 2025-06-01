23dfc1538f2b60a6e5aab0f97ab9a02974424042
Estifanos <estifanosbereket297@gmail.com>
2025-06-01 13:25:26 +0300

    context manager

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -1,6 +1,7 @@
 import asyncio
 import aiosqlite
 
+
 async def async_fetch_users():
   async with aiosqlite.connect("users.db") as db:
       async with db.execute("SELECT * FROM users;") as cursor:
@@ -9,12 +10,13 @@ async def async_fetch_users():
 
 async def async_fetch_older_users():
   async with aiosqlite.connect("users.db") as db:
-      async with db.execute("SELECT * FROM users WHERE age > 40;",) as cursor:
+      async with db.execute("SELECT * FROM users WHERE age > 40;") as cursor:
           rows = await cursor.fetchall()
           return rows
 
 
-[users, old_users] = asyncio.gather([
-  async_fetch_users(),
-  async_fetch_older_users(),
-])
+async def main():
+  users, old_users = await asyncio.gather(
+    async_fetch_users(),
+    async_fetch_older_users(),
+  )
