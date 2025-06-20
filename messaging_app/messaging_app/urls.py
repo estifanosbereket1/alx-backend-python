12f2a205aa8e0e00f9592949396bef32e2d55a1a
Estifanos <estifanosbereket297@gmail.com>
2025-06-20 23:05:59 +0300

    message app

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -2,7 +2,7 @@
 URL configuration for messaging_app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,13 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include("chats.urls")),
-    # Todo: explore what this is and how it works for auth
-    path("api-auth/", include("rest_framework.urls")),
+    path('admin/', admin.site.urls),
 ]
