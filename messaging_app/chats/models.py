322cbe82546475f8c6f7b4b0abafd0bff268f597
Estifanos <estifanosbereket297@gmail.com>
2025-06-21 13:49:44 +0300

    Chat app init

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    phone_number=models.CharField(max_length=15)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Conversation(models.Model):
+    participants = models.ManyToManyField(User, related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Message(models.Model):
+    sender =models.ForeignKey(User, on_delete=models.CASCADE,related_name="sender")
+    conversation =models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name="conversation")
+    content =models.TextField()
+    timestamp=models.DateTimeField(auto_now_add=True)
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'chats'
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'chats.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
