8c8dc9348888fe3a1441b3bb230f93988756da06
Estifanos <estifanosbereket297@gmail.com>
2025-06-24 21:39:59 +0300

    views

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import MessageViewSet, ConversationViewSet
 
-urlpatterns = [
-    path("")
-]
+router = DefaultRouter()
+
+router.register(r'messages', MessageViewSet)
+router.register(r'conversations', ConversationViewSet)
+
+urlpatterns = router.urls
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .serializers import UserSerializer, ConversationSerializer, MessageSerializer
 from .models import User, Conversation, Message
+from rest_framework import status, filters
 
 # Create your views here.
-class CoversationViewset(ModelViewSet):
+class ConversationViewSet(ModelViewSet):
     queryset=Conversation.objects.all()
     serializer_class=ConversationSerializer
 
