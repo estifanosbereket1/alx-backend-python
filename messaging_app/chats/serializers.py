19d16cbfddf4b3a71d90727e7dc0516f75369abf
Estifanos <estifanosbereket297@gmail.com>
2025-06-24 21:26:43 +0300

    validation

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -13,9 +13,28 @@ class ConversationSerializer(serializers.ModelSerializer):
         model = Conversation
         fields = ["conversation_id", "created_at", "participants"]
 
+# class MessageSerializer(serializers.ModelSerializer):
+#     sender=UserSerializer()
+#     conversation = ConversationSerializer()
+    
+#     class Meta:
+#         model =Message
+#         fields=["sender", "conversation", "message_body", "sent_at", "message_id"]
+
 class MessageSerializer(serializers.ModelSerializer):
-    sender=UserSerializer()
+    sender = UserSerializer()
     conversation = ConversationSerializer()
+    message_body = serializers.CharField(max_length=1000)
+    sender_full_name = serializers.SerializerMethodField()
+
     class Meta:
-        model =Message
-        fields=["sender", "conversation", "message_body", "sent_at", "message_id"]
+        model = Message
+        fields = ["sender", "sender_full_name", "conversation", "message_body", "sent_at", "message_id"]
+
+    def get_sender_full_name(self, obj):
+        return f"{obj.sender.first_name} {obj.sender.last_name}"
+
+    def validate_message_body(self, value):
+        if len(value.strip()) < 5:
+            raise serializers.ValidationError("Message is too short")
+        return value
